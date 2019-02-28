c6d4b05f00c768fbb3b9ba8d04af5f08109cd69b
umar <f14b150@ibitpu.edu.pk>
2019-02-28 20:39:34 +0500

    Updates

---
+++ precache-manifest.4b28d63e0aa1ea85adc88b8fb8269bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emptyapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bbd2bb71ca58d3cd1223",
+    "url": "/emptyapp/static/js/runtime~main.bbd2bb71.js"
+  },
+  {
+    "revision": "3664afe227fc42a1c254",
+    "url": "/emptyapp/static/js/main.3664afe2.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/emptyapp/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "3664afe227fc42a1c254",
+    "url": "/emptyapp/static/css/main.0ab03c40.chunk.css"
+  },
+  {
+    "revision": "58c3b22927f62e666fbc5958b8f80be9",
+    "url": "/emptyapp/index.html"
+  }
+];
